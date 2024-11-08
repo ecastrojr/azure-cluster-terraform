a7dd72f8c5643dab17c5ff6cde6e21ce34519c9d
Alex Castro <github@alexcastro.net.br>
2024-11-07 23:38:46 -0300

    adicionado info do compartilhamento

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,16 @@ output "master_public_ip" {
 output "worker_public_ips" {
   value = azurerm_public_ip.worker_ip[*].ip_address
 }
+
+output "username" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "share" {
+  value = azurerm_storage_share.ss.name
+}
+
+output "mount_password" {
+  value = azurerm_storage_account.sa.primary_access_key
+  sensitive = true
+}
